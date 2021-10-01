8c3c970810ac1e5714356a3b43dd55c972304c6c
Grant Kurtz <kurtz.grant@gmail.com>
2021-10-01 19:27:56 -0400

    Pulling out colors into separate function.

--- src/turtlestuff/TriangleSpiral.java
+++ src/turtlestuff/TriangleSpiral.java
@@ -3,6 +3,7 @@ package turtlestuff;
 public class TriangleSpiral {
 
 	static int[] colors = new int[512];
+	static Turtle t = new Turtle(1300, 1300);
 
 	public static void main(String[] args) throws InterruptedException {
 
@@ -10,28 +11,31 @@ public class TriangleSpiral {
 			colors[i] = i < 256 ? i : 256 - (i - 255);
 		}
 
-		Turtle t = new Turtle(1300, 1300);
 		t.left(90);
 		t.speed(50);
 
 		// Draw triangular
-		drawSpiral(t, 121, 4, 1.0, 600);
-//
-//		// Draw Square
+		drawSpiral(121, 4, 1.0, 600);
+
+		// Draw Square
 //		drawSpiral(t, 91, 3, 1.0, 700);
-//
-//		// Draw Hexagonal
+
+		// Draw Hexagonal
 //		drawSpiral(t, 61, 3, .3, 800);
 
 		// Draw Pentagon
 //		drawSpiral(t, 71, 3, .5, 800);
 	}
 
-	public static void drawSpiral(Turtle t, int angle, int step, double stepFactor, int lines) {
+	public static void drawSpiral(int angle, int step, double stepFactor, int lines) {
 		for(int i = 0; i < lines; i++) {
-			t.penColor(colors[(i+70)  % 512], colors[(i+140)  % 512], colors[(i+210)  % 512]);
+			colorize(i);
 			t.forward(step * i * stepFactor);
 			t.left(angle);
 		}
+	}
+
+	public static void colorize(int i) {
+		t.penColor(colors[(i+70)  % 512], colors[(i+140)  % 512], colors[(i+210)  % 512]);
 	}
 }
